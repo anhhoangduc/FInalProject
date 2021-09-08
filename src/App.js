c2336d12e7e51e9cd9dfd945e86fe8d346e32255
DESKTOP-CAGBJ2A\Duc Anh <anhhd.vnist@gmail.com>
2021-09-08 08:27:51 +0700

    submit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState({
+    student :['Huyen', 'Hoa', 'Hung', 'Long'],
+    count: 0
+  });
+
+  const previousElement= async () =>{
+    await setState({
+      ...state,
+      count: state.count - 1
+    });
+    if(state.count <= 0)
+      setState({
+        ...state,
+        count: 0
+      })
+  };
+  const nextElement= async () =>{
+    await setState({
+      ...state,
+      count: state.count + 1
+    });
+    if(state.count >=3)
+      setState({
+        ...state,
+        count: 3
+      })
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </a>
-      </header>
+      <p>学生一覧： [Huyen, Hoa, Hung, Long]</p>
+      <p>位置：{state.count + 1}</p>
+      <p>名前：{state.student[state.count]}</p>
+      <button onClick={() => nextElement()}>次に</button>
+      <button onClick={() => previousElement()}>前に</button>
     </div>
   );
 }
